0adc43c452a7a3a0aeac64a447dd6721b1ff4843
Chris Jones <cmsj@tenshu.net>
2020-07-02 14:35:42 +0100

    shrug

--- build_docs.sh
+++ build_docs.sh
@@ -11,6 +11,7 @@ fi
 
 mkdir -p .docs_tmp
 "${HAMMERSPOON_PATH}/scripts/docs/bin/build_docs.py" -e "${HAMMERSPOON_PATH}/scripts/docs/templates/" -o .docs_tmp -i "Hammerspoon Spoons" -j -t -n Source
-cp "${HAMMERSPOON_PATH}/scripts/docs/templates/{docs.css,jquery.js}" .docs_tmp/html/
+cp "${HAMMERSPOON_PATH}/scripts/docs/templates/docs.css" .docs_tmp/html/
+cp "${HAMMERSPOON_PATH}/scripts/docs/templates/jquery.js" .docs_tmp/html/
 mv .docs_tmp/html/* docs/
 mv .docs_tmp/docs{,_index}.json docs/
